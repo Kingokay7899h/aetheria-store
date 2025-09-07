f8e844230ab0f151bc4051166db2c30c2045bb3f
Kingokay7899h <shreyashdesai60@gmail.com>
2025-09-08 00:57:36 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AetheriaClone from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AetheriaClone from './App.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AetheriaClone />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
